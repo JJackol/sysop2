7954c01208b5fd466b675068d88fbb34a0f8da1e
JJackol <jacekczerwonka@gmail.com>
2020-03-09 11:59:42 +0100

    koniec lab1

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-echo $@
 
 if [ -d ${1} ]
 then
 	prefix=${1}
 	for filee in `ls  ${prefix}`
 	do
+		echo ${prefix}/${filee}
 		if [ -d ${prefix}/${filee}  ]
 		then
 			infix=${filee}
-			echo aaaaaaa ${infix}
 			for filee in `ls  ${prefix}/${infix}`
 			do
-				if [ -f ${prefix}/${infix}/${filee}  ]
-				then
-					echo ${prefix}/${infix}/${filee}
-				fi
+				echo ${prefix}/${infix}/${filee}
 			done
-		else
-			echo ${prefix}/${filee}
 		fi
 	done
 fi
